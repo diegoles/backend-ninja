752c5c078f4dd59d7d0d78bc9c309bf5e3ab1b2d
Edgar Diaz <diaz_eda@hotmail.com>
2019-01-26 16:49:05 -0500

    Agregando configuración y controlador para poder desplegar el tiempo que se demora en realizar una petición.

---
+++ src/main/java/com/udemy/controller/RequestTimeInterceptor.java
@@ -0,0 +1,36 @@
+package com.udemy.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component("requestTimeInterceptor")
+public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
+
+	private final static Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
+
+	// PRIMERO
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		request.setAttribute("startTime", System.currentTimeMillis());
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+		long startTime = (long) request.getAttribute("startTime");
+		LOG.info("REQUEST URL: '" + request.getRequestURL().toString() + "' TOTAL TIME: '"
+				+ (System.currentTimeMillis() - startTime) + "'ms");
+
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+}

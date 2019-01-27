43fda131372d4c98d3e3a9f1176483a6980ef889
Edgar Diaz <diaz_eda@hotmail.com>
2019-01-27 13:29:23 -0500

    Agregando plantillas y controlador para un login con su modelo.

---
+++ src/main/java/com/udemy/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.udemy.model.UserCredential;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/")
+	public String redirectToLogin() {
+		return "redirect:/login";
+	}
+
+	@GetMapping("/login")
+	public String showLoginForm(Model model,
+			@RequestParam(name = "error", required = false) String error) {
+		model.addAttribute("userCredential", new UserCredential());
+		model.addAttribute("error", error);
+		return "login";
+	}
+
+	@PostMapping("/loginCheck")
+	public String loginCheck(@ModelAttribute(name = "userCredential") UserCredential userCredential) {
+
+		if ("user".equals(userCredential.getUsername()) && "user".equals(userCredential.getPassword())) {
+			return "contacts";
+		}
+		return "redirect:/login?error";
+	}
+}

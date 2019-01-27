45f4e9aa767f1d925ede9823c044ce65ed53c2d9
Edgar Diaz <diaz_eda@hotmail.com>
2019-01-27 13:32:58 -0500

    COmentando interceptor para controlar un error interno 500.

--- src/main/java/com/udemy/controller/ErrorsController.java
+++ src/main/java/com/udemy/controller/ErrorsController.java
@@ -3,12 +3,12 @@ package com.udemy.controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class ErrorsController {
 	// El error 400 por defecto ya se coge por configuracion ya que existe la carpeta error
 	private final static String ERROR_VIEW = "error/500";
 	
-	@ExceptionHandler(Exception.class)
+//	@ExceptionHandler(Exception.class)
 	public String showInternalError() {
 		return ERROR_VIEW;
 	}
